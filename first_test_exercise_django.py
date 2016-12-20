ec69c4f2da7c6862ea2fc389bdd5d2f458c8720d
Lex98 <Lexis098@gmail.com>
2016-12-20 22:27:47 +0300

    Update first_test_exercise_django.py

--- first_test_exercise_django.py
+++ first_test_exercise_django.py
@@ -1,15 +1,16 @@
 from django.db import models
 
 
-# Create your models here.
 class User(models.Model):
-    user_name = models.CharField('Username', max_length=50)
+    """User model"""
+    username = models.CharField('Username', max_length=50)
 
     def __str__(self):
-        return self.user_name
+        return self.username
 
 
 class Device(models.Model):
+    """Device model"""
     title = models.CharField(max_length=50)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
